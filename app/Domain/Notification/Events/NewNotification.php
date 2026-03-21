51342639979be399fbb032c06a68998989859c4c
Ahmed Mostafa <lofylofy56@gmail.com>
2026-03-21 04:28:18 +0200

    ...

--- app/Domain/Notification/Events/NewNotification.php
+++ app/Domain/Notification/Events/NewNotification.php
@@ -12,7 +12,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class NewNotification implements ShouldBroadcast
+class newNotification implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
