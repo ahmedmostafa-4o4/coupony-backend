2ba0a258a2400486577359001e4406f0bf2d717c
Ahmed Mostafa <lofylofy56@gmail.com>
2026-04-12 14:37:39 +0200

    Update onboarding completion check to prioritize header role over input role

--- app/Application/Http/Resources/UserResource.php
+++ app/Application/Http/Resources/UserResource.php
@@ -33,7 +33,7 @@ class UserResource extends JsonResource
                 'gender' => $this->profile?->gender
             ]),
             'is_store_owner' => $this->stores()->exists(),
-            'is_onboarding_completed' => $this->isOnboardingCompleted($this->id, $request->input('role')),
+            'is_onboarding_completed' => $this->isOnboardingCompleted($this->id, $request->header('X-User-Role') ?? $request->input('role') ?? $this->roles?->first()?->name),
 
             'sessions' => $this->whenLoaded('sessions', [
                 'token' => $this->sessions?->first()?->token,
